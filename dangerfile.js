ee9217b1ca24172845717000dc54197064bc170c
Lucas Cordeiro <lucas.cordeiro5@etec.sp.gov.br>
2020-09-18 00:46:45 -0300

    Update dangerfile.js

--- dangerfile.js
+++ dangerfile.js
@@ -1,8 +1,4 @@
 import {danger, fail, message, warn, schedule} from 'danger'
 
-// Add a CHANGELOG entry for app changes
-const hasChangelog = danger.git.modified_files.includes("CHANGELOG.yml")
 
-if (!hasChangelog) {
   fail("Please add a changelog entry for your changes. :crystall_ball:")
-}
